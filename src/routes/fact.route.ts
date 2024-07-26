a0bfb6d38cbe2a0c9f20277a22e291b8b08b14e5
dashainCodes <dashain2.07@gmail.com>
2024-07-26 15:15:03 +0545

    fix

--- src/routes/fact.route.ts
+++ src/routes/fact.route.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/", [ validate(createFactSchema)], createFactHandler);
 router.patch("/:factId",  updateFactHandler);
 router.get("/:factId", [validate(getFactSchema)], getFactHandler);
-router.get("/by-expiditionId/:factId", [validate(getFactSchema)], getFactByExpeditionHandler);
+router.get("/by-expiditionId/:expeditionId", [validate(getFactSchema)], getFactByExpeditionHandler);
 router.get("/", getAllFactHandler);
 router.delete("/:factId", [ validate(deleteFactSchema)], deleteFactHandler);
 
