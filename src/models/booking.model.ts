f9e497cf3e3073cb0a405b004213b88a78595589
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-08-02 15:54:29 +0545

    build

--- src/models/booking.model.ts
+++ src/models/booking.model.ts
@@ -1,158 +1,3 @@
-// import mongoose from "mongoose";
-// import { customAlphabet } from "nanoid";
-// import { ExpeditionDocument } from "./expedition";
-// import { ActivityDocument } from "./activity.model";
-
-// const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 10);
-
-// export interface emergency {
-//   fullName: String;
-//   contact: String;
-//   relationship: String;
-// }
-
-// export const emergencyContactSchema = new mongoose.Schema({
-//   fullName: String,
-//   contact: String,
-//   relationship: String,
-
-// })
-
-// export interface transportation {
-//   method: String;
-//   transportationCharge: Number;
-// }
-
-// export const transportationSchema = new mongoose.Schema({
-//   method: String,
-//   transportationCharge: Number,
-// })
-
-// export interface children {
-//   age:Number;
-//   no: Number;
-// }
-
-// export const childrenSchema = new mongoose.Schema({
-//   age:Number,
-//   no: Number
-// })
-
-// export interface travelInsurance {
-//   insurance: Boolean;
-//   insuranceCharge: Number;
-// }
-
-// export const travelInsuranceSchema = new mongoose.Schema({
-//   insurance: Boolean,
-//   insuranceCharge: Number,
-// })
-
-// export interface price {
-//   tripPrice:Number;
-//   transportationCharge: Number;
-//   insuranceCharge:Number;
-//   tax:Number;
-//   bankCharge:Number;
-//   totalCharge:Number;
-// }
-
-// export const priceSchema = new mongoose.Schema({
-//   tripPrice:Number,
-//   transportationCharge: Number,
-//   insuranceCharge:Number,
-//   tax:Number,
-//   bankCharge:Number,
-//   totalCharge:Number
-// })
-
-// export interface payment {
-//   paymentMethod:String;
-//   amountPaid: Number;
-// }
-
-// export const paymentSchema = new mongoose.Schema({
-//   paymentMethod:String,
-//   amountPaid: Number
-// })
-
-// export interface BookingInput {
-//   fullName: string;
-//   email: string;
-//   phone: string;
-//   dob?: Date;
-//   gender?: string;
-//   departureDate?: Date;
-//   noOfAdults?: number;
-//   noOfChildren?:children[];
-//   passportNo?: string;
-//   emergencyContact?: emergency;
-//   transportation?: transportation;
-//   travelInsurance?:travelInsurance;
-//   price?:price;
-//   payment?:payment
-//   expedition?: ExpeditionDocument["_id"];
-//   package?: string;
-//   notes?: string;
-//   status?: string;
-//   paymentStatus?: string;
-//   isSeen?: boolean;
-// }
-
-// export interface BookingDocument extends BookingInput, mongoose.Document {
-//   createdAt: Date;
-//   updatedAt: Date;
-// }
-
-// const bookingSchema = new mongoose.Schema(
-//   {
-//     bookingId: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       default: () => `booking_${nanoid()}`,
-//     },
-//     fullName: { type: String, required: true },
-//     email: { type: String, required: true },
-//     phone: { type: String, required: true },
-//     noOfAdults: { type: Number},
-//     notes: { type: String },
-//     package: { type: String },
-//     expedition: { type: mongoose.Schema.Types.ObjectId, ref: "Expedition" },
-//     status: { type: String, enum: ["active", "canceled"], default: "active" },
-//     dob: { type: Date},
-//     departureDate: { type: Date},
-//     passportNo: { type: String},
-//     gender: { type: String},
-//     emergencyContact: {
-//       type: emergencyContactSchema
-//     },
-//     noOfChildren: {
-//       type: Array(childrenSchema)
-//     },
-//     transportation: {
-//       type: transportationSchema
-//     },
-//     travelInsurance: {
-//       type: travelInsuranceSchema
-//     },
-//     price: {
-//       type: priceSchema
-//     },
-//     payment: {
-//       type: paymentSchema
-//     },
-//   },
-
-//   {
-//     timestamps: true,
-//   }
-// );
-
-// const BookingModel = mongoose.model<BookingDocument>("Booking", bookingSchema);
-
-// export default BookingModel;
-
 import mongoose from "mongoose";
 import { customAlphabet } from "nanoid";
 import { ExpeditionDocument } from "./expedition";
@@ -178,6 +23,7 @@ export interface BookingInput {
   emergencyName: string;
   emergencyPhone: number;
   emergencyRelationship: string;
+  paymentOption: "full-payment" | "deposit-payment";
 }
 
 export interface BookingDocument extends BookingInput, mongoose.Document {
@@ -207,6 +53,11 @@ const BookingSchema = new mongoose.Schema(
     emergencyName: { type: String, required: true },
     emergencyPhone: { type: Number, required: true },
     emergencyRelationship: { type: String, required: true },
+    paymentOption: {
+      type: String,
+      required: true,
+      enum: ["full-payment", "deposit-payment"],
+    },
 
     expedition: { type: mongoose.Schema.Types.ObjectId, ref: "Expedition" },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
