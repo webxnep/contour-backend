76a6d92af8c0ceaf7ee1dcd72ef736f1489d2e76
dashainCodes <dashain2.07@gmail.com>
2024-08-09 16:39:41 +0545

    fix

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -10,7 +10,8 @@ const operation = retry.operation({
   maxTimeout: 5000, // Maximum time between retries (in milliseconds)
   randomize: true, // Randomize the timeouts
 });
-const {connection } = require('../config');
+// const {connection } = require('../config');
+const connection=process.env.DB_CONNECTION||""
 export default async function connectDB() {
   operation.attempt(async () => {
     try {
